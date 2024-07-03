e500dee7937709ba946ff8df2eea3bacbd74b026
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2024-07-03 12:23:31 +0000

    Dashboard update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+     
+        <Routes>
+        <Route path="/" element={<Navbar />} />
+        </Routes>
 
-      <Navbar />
-    </div>
+    </BrowserRouter>
   );
 }
 
