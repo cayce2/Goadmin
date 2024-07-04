7e813a57c4fd36ed3ac285595be7da3fd8b93bb7
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2024-07-04 11:49:17 +0000

    routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Dashboard from "./components/Dashboard";
-import Calendar from "./pages/calender"
+import Dashboard from './components/Dashboard'
+import Calendar from './pages/calender';
 
 function App() {
-  return (
-    <BrowserRouter>
-     
-        <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/" element={<Calendar />} />
-        </Routes>
-
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/calendar" element={<Calendar />} />                 
+                </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
