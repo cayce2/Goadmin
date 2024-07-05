b04b845eba2051e7d6a0b50433cf02ab7e5007f1
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2024-07-05 07:13:07 +0000

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Appbar from './components/appbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function  app() {
+  return(
+    <div className='nav'>
+      <Appbar/>
     </div>
-  );
+  )
 }
-
-export default App;
