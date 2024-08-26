77a1bedac98e9880b2e9a8256ffbc1a29146d464
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2024-08-26 17:24:44 +0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -27,9 +27,9 @@ function classNames(...classes) {
 export default function Navbar() {
   return (
     <>
-      <div className="min-h-full, w-full">
-        <Disclosure as="nav" className="bg-gray-800">
-          <div className="mx-auto px-4  sm:px-6 lg:px-8">
+      div className="min-h-full w-full">
+       <Disclosure as="nav" className="bg-gray-800 w-full">
+         <div className="w-full px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
@@ -158,13 +158,13 @@ export default function Navbar() {
           </DisclosurePanel>
         </Disclosure>
 
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+       <header className="bg-white shadow w-full">
+          <div className="w-full px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
           </div>
         </header>
-        <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+        <main className="w-full">
+          <div className="w-full px-4 py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
         </main>
       </div>
     </>
